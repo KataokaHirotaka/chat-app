1f3a16290ad7df44011282aad6bbccb68585f79b
HirotakaKataoka <h-kataoka@bfp.co.jp>
2022-11-08 11:40:52 +0900

    環境構築

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+
+const Page: NextPage = () => {
+  return (
+    <Box>
+      <Heading>CHAKRA UI</Heading>
+    </Box>
+  )
+}
+
+export default Page
